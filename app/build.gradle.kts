257761f1b0c477efdcf8603232822789a38142a9
kaorun <hotarunichijou@ik.me>
2025-08-31 19:24:04 +0300

    build: prepare for v1.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "org.kaorun.diary"
         minSdk = 31
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.1.0"
+        versionCode = 6
+        versionName = "1.2.0"
 
 		setProperty("archivesBaseName", "Diary-$versionName")
     }

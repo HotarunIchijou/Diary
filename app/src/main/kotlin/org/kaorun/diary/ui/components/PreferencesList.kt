4a4ace0f4a20cd450a8b8da274c5f2ec5be3432b
kaorun <hotarunichijou@ik.me>
2026-01-08 22:08:53 +0300

    feat: UI updates and tweaks to follow MDC defaults

--- app/src/main/kotlin/org/kaorun/diary/ui/components/PreferencesList.kt
+++ app/src/main/kotlin/org/kaorun/diary/ui/components/PreferencesList.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.util.AttributeSet
 import androidx.preference.Preference
 import androidx.preference.PreferenceViewHolder
+import androidx.recyclerview.widget.RecyclerView
 import org.kaorun.diary.R
 import org.kaorun.diary.databinding.ItemSettingBinding
 
@@ -20,11 +21,15 @@ class PreferencesList @JvmOverloads constructor(
         super.onBindViewHolder(holder)
 
         val binding = ItemSettingBinding.bind(holder.itemView)
+        
+        val recyclerView = holder.itemView.parent as? RecyclerView
+        val position = recyclerView?.getChildAdapterPosition(holder.itemView) ?: 0
+        val itemCount = recyclerView?.adapter?.itemCount ?: 1
 
         binding.title.text = title
         binding.summary.text = summary
         binding.icon.setImageDrawable(icon)
 
-        binding.listItemLayout.updateAppearance(1, 1)
+        binding.listItemLayout.updateAppearance(position, itemCount)
     }
 }
